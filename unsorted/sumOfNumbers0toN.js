cf22d311a3dceb5f1ac67836efa9c96f881ba65e
Arnold Son <hyomin.son@alum.utoronto.ca>
2017-05-28 16:01:04 -0700

    added sumOfNumbers0toN.js

---
+++ unsorted/sumOfNumbers0toN.js
@@ -0,0 +1,12 @@
+var SequenceSum = (function() {
+    function SequenceSum() {}
+
+    SequenceSum.showSequence = function(count) {
+        for(var i = 0; i <= count; i++){
+
+        }
+    };
+
+    return SequenceSum;
+
+})();
