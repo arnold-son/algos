b38be107c7844e3f1f5f917eced038d10e48807e
Hyo Min Son <arnold@Hyos-MBP.attlocal.net>
2017-05-29 19:54:45 -0700

    changed SLQ.js, updated SLStack functions: 'compareStacks' added 'SLStack.copy()'

--- codingDojo/chapter6/SLQ.js
+++ codingDojo/chapter6/SLQ.js
@@ -1,160 +1,162 @@
-function SLNode (val) {
-    this.val = val;
-    this.next = null;
-}
-
-function SLQueue () {
-    var head = null;
-    var tail = null;
-    this.display = function(){
-        if(!head){ console.log("Head -> [null] <- Tail"); return this}
-        var str = "Head"
-        var runner = head;
-        while(runner){
-            str+= " -> " + "[" + runner.val + "]"
-            runner = runner.next;
+module.exports = function SLQueue() {
+        function SLNode (val) {
+            this.val = val;
+            this.next = null;
         }
-        str+= " <- Tail"
-        console.log(str);
-        return this;
-    }
-    this.enqueue = function(val){
-        if(!head){
-            head = new SLNode(val);
-            tail = head;
-        } else {
-            tail.next = new SLNode(val);
-            tail = tail.next;
-        }
-        return this;
-    }
-    this.dequeue = function(){
-        if(!head){ return null}
-        var temp = head;
-        head = head.next;
-        return temp;
-    }
-    this.front = function(){
-        return head;
-    }
-    this.contains = function(val){
-        var runner = head;
-        while(runner){
-            if(runner.val === val){return true}
-            runner = runner.next;
-        }
-        return false;
-    }
-    this.isEmpty = function(){
-        return head === null;
-    }
-    this.size = function(){
-        if(!head){return 0}
-        var size = 0;
-        var runner = head;
-        while(runner){
-            size++;
-            runner = runner.next;
-        }
-        return size;
-    }
-    this.compareQueues = function(otherQueue){
-        if(!head && !otherQueue.front()){ return true }
-        var runner1 = head;
-        var runner2 = otherQueue.front();
-        while(runner1 || runner2){
-            if(!runner1 || !runner2){return false}
-            else if(runner1.val !== runner2.val){return false}
-            runner1 = runner1.next;
-            runner2 = runner2.next;
-        }
-        return true;
-    }
-    this.removeMins = function(){
-        if(!head){console.log("Empty list");return this}
-        var min = head.val;
-        var runner = head.next;
-        while(runner){
-            if(runner.val < min){
-                min = runner.val;
-            }
-            runner = runner.next;
-        }
-        while(head.val === min){head = head.next};
-        runner = head;
-        while(runner.next){
-            if(runner.next.val === min){
-                runner.next = runner.next.next;
-            } else{
+        var head = null;
+        var tail = null;
+        this.display = function(){
+            if(!head){ console.log("Head -> [null] <- Tail"); return this}
+            var str = "Head"
+            var runner = head;
+            while(runner){
+                str+= " -> " + "[" + runner.val + "]"
                 runner = runner.next;
             }
+            str+= " <- Tail"
+            console.log(str);
+            return this;
         }
-        console.log("The minimum value " + min + " was removed.")
-        return this;
-    }
-    this.removeLastMin = function(){
-        if(!head){console.log("Empty list");return this}
-        var min = head.val;
-        var runner = head.next;
-        while(runner){
-            if(runner.val < min){
-                min = runner.val;
+        this.enqueue = function(val){
+            if(!head){
+                head = new SLNode(val);
+                tail = head;
+            } else {
+                tail.next = new SLNode(val);
+                tail = tail.next;
             }
-            runner = runner.next;
+            return this;
         }
-        var nodePrevToLastMin;
-        runner = head;
-        while(runner.next){
-            if(runner.next.val === min){
-                nodePrevToLastMin = runner;
+        this.dequeue = function(){
+            if(!head){ return null}
+            var temp = head.val;
+            head = head.next;
+            return temp;
+        }
+        this.front = function(){
+            return head;
+        }
+        this.contains = function(val){
+            var runner = head;
+            while(runner){
+                if(runner.val === val){return true}
+                runner = runner.next;
             }
-            runner = runner.next;
-        }
-        if(nodePrevToLastMin){nodePrevToLastMin.next = nodePrevToLastMin.next.next}
-        else{head = head.next}
-        console.log("The last minimum value " + min + " was removed.")
-        return this;
-    }
-    this.interleaveQueue = function(){
-        if(this.size() < 3){console.log("Must contain at least 3 elements to interleave.");return this}
-        var runner = head;
-        var runner2 = head;
-        var secondHalf = Math.ceil(this.size()/2);
-        for(var i = 1; i <= secondHalf; i++){
-            if(secondHalf % 2 > 0 && i === secondHalf){tail = runner2}
-            runner2 = runner2.next;
-        }
-        while(runner2){
-            var temp = runner.next;
-            runner.next = runner2;
-            runner2 = runner2.next;
-            runner.next.next = temp;
-            runner = temp;
-        }
-        tail.next = null;
-        return this;
-    }
-}
-function compareQueues(queue1,queue2){
-    if(!queue1.front() && !queue2.front()){
-        return true;
-    } else if(!queue1.front() || !queue2.front()){
-        return false;
-    }
-    var runner1 = queue1.front();
-    var runner2 = queue2.front();
-    while(runner1 || runner2){
-        if(!runner1 || !runner2){
-            return false;
-        } else if(runner1.val !== runner2.val){
             return false;
         }
-        runner1 = runner1.next;
-        runner2 = runner2.next;
+        this.isEmpty = function(){
+            return head === null;
+        }
+        this.size = function(){
+            if(!head){return 0}
+            var size = 0;
+            var runner = head;
+            while(runner){
+                size++;
+                runner = runner.next;
+            }
+            return size;
+        }
+        this.compareQueues = function(otherQueue){
+            if(!head && !otherQueue.front()){ return true }
+            var runner1 = head;
+            var runner2 = otherQueue.front();
+            while(runner1 || runner2){
+                if(!runner1 || !runner2){return false}
+                else if(runner1.val !== runner2.val){return false}
+                runner1 = runner1.next;
+                runner2 = runner2.next;
+            }
+            return true;
+        }
+        this.removeMins = function(){
+            if(!head){console.log("Empty list");return this}
+            var min = head.val;
+            var runner = head.next;
+            while(runner){
+                if(runner.val < min){
+                    min = runner.val;
+                }
+                runner = runner.next;
+            }
+            while(head.val === min){head = head.next};
+            runner = head;
+            while(runner.next){
+                if(runner.next.val === min){
+                    runner.next = runner.next.next;
+                } else{
+                    runner = runner.next;
+                }
+            }
+            console.log("The minimum value " + min + " was removed.")
+            return this;
+        }
+        this.removeLastMin = function(){
+            if(!head){console.log("Empty list");return this}
+            var min = head.val;
+            var runner = head.next;
+            while(runner){
+                if(runner.val < min){
+                    min = runner.val;
+                }
+                runner = runner.next;
+            }
+            var nodePrevToLastMin;
+            runner = head;
+            while(runner.next){
+                if(runner.next.val === min){
+                    nodePrevToLastMin = runner;
+                }
+                runner = runner.next;
+            }
+            if(nodePrevToLastMin){nodePrevToLastMin.next = nodePrevToLastMin.next.next}
+            else{head = head.next}
+            console.log("The last minimum value " + min + " was removed.")
+            return this;
+        }
+        this.interleaveQueue = function(){
+            if(this.size() < 3){console.log("Must contain at least 3 elements to interleave.");return this}
+            var runner = head;
+            var runner2 = head;
+            var secondHalf = Math.ceil(this.size()/2);
+            for(var i = 1; i <= secondHalf; i++){
+                if(secondHalf % 2 > 0 && i === secondHalf){tail = runner2}
+                runner2 = runner2.next;
+            }
+            while(runner2){
+                var temp = runner.next;
+                runner.next = runner2;
+                runner2 = runner2.next;
+                runner.next.next = temp;
+                runner = temp;
+            }
+            tail.next = null;
+            return this;
+        }
     }
-    return true;
-}
-var queue = new SLQueue();
-var queue2 = new SLQueue();
-queue.enqueue(1).enqueue(2).enqueue(3).enqueue(4).enqueue(5).display();
-queue.interleaveQueue().display()
+    // function compareQueues(queue1,queue2){
+    //     if(!queue1.front() && !queue2.front()){
+    //         return true;
+    //     } else if(!queue1.front() || !queue2.front()){
+    //         return false;
+    //     }
+    //     var runner1 = queue1.front();
+    //     var runner2 = queue2.front();
+    //     while(runner1 || runner2){
+    //         if(!runner1 || !runner2){
+    //             return false;
+    //         } else if(runner1.val !== runner2.val){
+    //             return false;
+    //         }
+    //         runner1 = runner1.next;
+    //         runner2 = runner2.next;
+    //     }
+    //     return true;
+    // }
+    // return SLQueue;
+
+
+// var queue = new SLQueue();
+// var queue2 = new SLQueue();
+// queue.enqueue(1).enqueue(2).enqueue(3).enqueue(4).enqueue(5).display();
+// queue.interleaveQueue().display()
--- codingDojo/chapter6/SLStack.js
+++ codingDojo/chapter6/SLStack.js
@@ -1,9 +1,22 @@
+var SLQueue = require("./SLQ.js");
+
 function SLStack(){
     function SLNode(val){
         this.val = val;
         this.next = null;
     }
-    var top;
+    var top = null;
+    this.display = function(){
+        if(top === null){ console.log("Top -> [null]"); return this}
+        var str = "Top -> "
+        var runner = top;
+        while(runner){
+            str+= "[" + runner.val + "]" + "\n" + "       ";
+            runner = runner.next;
+        }
+        console.log(str);
+        return this;
+    }
     this.push = function(val){
         if(!top){
             top = new SLNode(val)
@@ -16,15 +29,23 @@ function SLStack(){
     }
     this.pop = function(){
         if(!top){
-            return
+            console.log("Empty stack. Nothing to pop.")
+            return null;
         } else{
-            var temp = top;
+            var poppedValue = top.val;
             top = top.next;
-            return temp;
+            // console.log("Popped value: " + poppedValue)
+            return poppedValue;
         }
     }
     this.top = function(){
-        return top;
+        if(top === null){
+            console.log("Empty stack.")
+            return null;
+        } else{
+            console.log("Top value: " + top.val)
+            return top.val;
+        }
     }
     this.contains = function(val){
         if(!top){return false}
@@ -37,8 +58,7 @@ function SLStack(){
         return false;
     }
     this.isEmpty = function(){
-        if(!top){return true}
-        return false;
+        return top === null;
     }
     this.size = function(){
         if(!top){return 0}
@@ -50,23 +70,73 @@ function SLStack(){
         }
         return size;
     }
-    this.compareStacks(secondStack){
-        if(!top && !secondStack.top()){
-            return true;
-        } else if(!top || !secondStack.top()){
-            return false;
+    this.copy = function(){
+        var storage = new SLQueue();
+        var newCopy = new SLStack();
+        while(!this.isEmpty()){
+            newCopy.push(this.pop());
         }
-        var runner1 = top;
-        var runner2 = secondStack.top();
-        while(runner1 || runner2){
-            if(!runner1 || runner2){
-                return false
-            } else if(runner1.val !== runner2.val){
-                return false;
-            }
-            runner1 = runner1.next;
-            runner2 = runner2.next;
+        while(!newCopy.isEmpty()){
+            var temp = newCopy.pop();
+            this.push(temp);
+            storage.enqueue(temp);
         }
+        while(!storage.isEmpty()){
+            newCopy.push(storage.dequeue());
+        }
+        return newCopy;
+    }
+    // this.compareStacks = function(secondStack){
+    //     if(!top && !secondStack.top()){
+    //         return true;
+    //     } else if(!top || !secondStack.top()){
+    //         return false;
+    //     }
+    //     var runner1 = top;
+    //     var runner2 = secondStack.top();
+    //     while(runner1 || runner2){
+    //         if(!runner1 || runner2){
+    //             return false
+    //         } else if(runner1.val !== runner2.val){
+    //             return false;
+    //         }
+    //         runner1 = runner1.next;
+    //         runner2 = runner2.next;
+    //     }
+    //     return true;
+    // }
+
+}
+
+function compareStacks(stack1,stack2){
+
+    if(stack1.top() === null && stack2.top() === null){
         return true;
     }
+    if(stack1.size() !== stack2.size()){
+        return false;
+    }
+    var storage = new SLStack();
+    var stacksAreEqual = true;
+    while(stacksAreEqual && !stack1.isEmpty()){
+        var stack1Value = stack1.pop();
+        var stack2Value = stack2.pop();
+        if(stack1Value !== stack2Value){
+            stacksAreEqual = false;
+        }
+        storage.push(stack1Value).push(stack2Value);
+    }
+    while(!storage.isEmpty()){
+        stack2.push(storage.pop());
+        stack1.push(storage.pop());
+    }
+
+    stacksAreEqual ? console.log("The stacks are equal.") : console.log("The stacks are not equal.")
+
+    return stacksAreEqual;
 }
+var one = new SLStack().push(1).push(2).push(3).display();
+var copy = one.copy().display();
+one.display();
+copy.display();
+compareStacks(one,copy)
