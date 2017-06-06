0c9c3f7a5e04ce6d418936cef3ff06665236fcf3
Hyo Min Son <arnold@Hyos-MBP.attlocal.net>
2017-06-06 11:44:27 -0700

    aChainAddingFunction.js

---
+++ unsorted/codewars/unsolved/aChainAddingFunction.js
@@ -0,0 +1,21 @@
+// function add(n){
+//     return add;
+// }
+//
+
+
+//codewars solution #1:
+// function add(n){
+//   var fn = function(x) {
+//     return add(n + x);
+//   };
+//
+//   fn.valueOf = function() {
+//     return n;
+//   };
+//
+//   return fn;
+// }
+
+//references:
+//Object.prototype.valueOf(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf
