2f401dab36808b7b1768a0f6f8d34750d0770367
Arnold Son <arnold.son@icloud.com>
2017-08-15 08:36:52 -0700

    Java Factorial

---
+++ codewars/Java/Factorial/src/com/company/BestSolution.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class BestSolution {
+    public int factorial(int n) {
+        if(n < 0 || n > 12) throw new IllegalArgumentException("Useless fuffery!");
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+}
